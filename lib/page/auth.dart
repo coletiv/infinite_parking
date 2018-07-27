5f3dced7757d271ea33eb0bb2a5328d68e4e9b77
Antonio Valente <antonio@coletiv.com>
2018-07-27 15:17:52 +0100

    Auth page with loading state

--- lib/page/auth.dart
+++ lib/page/auth.dart
@@ -1,4 +1,4 @@
-import 'package:coletiv_infinite_parking/network/network.dart';
+import 'package:coletiv_infinite_parking/network/client/auth_client.dart';
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatefulWidget {
@@ -7,60 +7,100 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
+  BuildContext _scaffoldContext;
+
+  final progressKey = GlobalKey();
+  final loginBtnKey = GlobalKey();
+
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
-  void login() async {
+  bool isLoading = false;
+
+  void _login() async {
+    _updateLoadingState(true);
+
     final email = emailController.text;
     final password = passwordController.text;
 
-    final session = await Network.login(email, password);
+    final isLoggedIn = await authClient.login(email, password);
 
-    print(session);
+    if (isLoggedIn) {
+      // TODO go to new screen
+    } else {
+      _showError();
+    }
+  }
 
-    // TODO save session and go to new screen
+  void _showError() {
+    _updateLoadingState(false);
+
+    final snackBar = SnackBar(content: Text("Authentication Error"));
+    Scaffold.of(_scaffoldContext).showSnackBar(snackBar);
+  }
+
+  void _updateLoadingState(bool isLoading) {
+    setState(() {
+      this.isLoading = isLoading;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Authentication'),
-      ),
-      body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 20.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(bottom: 16.0),
-                child: TextField(
-                  maxLines: 1,
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    hintText: 'Email',
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: 16.0),
-                child: TextField(
-                  maxLines: 1,
-                  obscureText: true,
-                  controller: passwordController,
-                  decoration: InputDecoration(hintText: 'Password'),
-                ),
-              ),
-              RaisedButton(
-                onPressed: login,
-                child: Text('LOGIN'),
-              )
-            ],
-          ),
+        appBar: AppBar(
+          title: Text('Authentication'),
         ),
-      ),
-    );
+        body: Builder(
+          builder: (BuildContext context) {
+            _scaffoldContext = context;
+            return Container(
+              margin: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(bottom: 16.0),
+                      child: TextField(
+                        maxLines: 1,
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 16.0),
+                      child: TextField(
+                        maxLines: 1,
+                        obscureText: true,
+                        controller: passwordController,
+                        decoration: InputDecoration(hintText: 'Password'),
+                      ),
+                    ),
+                    Stack(
+                      alignment: AlignmentDirectional.center,
+                      children: <Widget>[
+                        Opacity(
+                          opacity: isLoading ? 1.0 : 0.0,
+                          child: CircularProgressIndicator(),
+                        ),
+                        Opacity(
+                          opacity: isLoading ? 0.0 : 1.0,
+                          child: RaisedButton(
+                            onPressed: _login,
+                            child: Text('LOGIN'),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+        ));
   }
 
   @override
