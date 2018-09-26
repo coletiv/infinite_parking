3b46c35cba5170a757f0fdd4b023a6435fc02482
Antonio Valente <antonio@coletiv.com>
2018-09-26 15:25:31 +0100

    session manager refactor

--- lib/data/session_manager.dart
+++ lib/data/session_manager.dart
@@ -4,6 +4,8 @@ import 'dart:convert';
 import 'package:coletiv_infinite_parking/data/model/auth_token.dart';
 import 'package:coletiv_infinite_parking/data/model/fare.dart';
 import 'package:coletiv_infinite_parking/data/model/fare_cost.dart';
+import 'package:coletiv_infinite_parking/data/model/municipal_zone.dart';
+import 'package:coletiv_infinite_parking/data/model/vehicle.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final sessionManager = _SessionManager._internal();
@@ -14,35 +16,41 @@ class _SessionManager {
   final String _authTokenKey = 'AuthToken';
   final String _emailKey = "Email";
   final String _passwordKey = "Password";
-  final String _fareTokenKey = "FareToken";
+  final String _selectedVehicleKey = "SelectedVehicle";
+  final String _selectedZoneTokenKey = "SelectedZoneToken";
+  final String _selectedFareTokenKey = "SelectedFareToken";
   final String _selectedFaresKeys = "SelectedFares";
 
+  Future<SharedPreferences> _getPrefs() async =>
+      await SharedPreferences.getInstance();
+
   Future<bool> _saveAuthToken(AuthToken authToken) async {
-    final prefs = await SharedPreferences.getInstance();
     final String authTokenJson = json.encode(authToken);
-    return await prefs.setString(_authTokenKey, authTokenJson);
+    return await _getPrefs()
+        .then((prefs) => prefs.setString(_authTokenKey, authTokenJson));
   }
 
   Future<bool> _saveEmail(String email) async {
-    final prefs = await SharedPreferences.getInstance();
-    return await prefs.setString(_emailKey, email);
+    return await _getPrefs().then((prefs) => prefs.setString(_emailKey, email));
   }
 
   Future<bool> _savePassword(String password) async {
-    final prefs = await SharedPreferences.getInstance();
-    return await prefs.setString(_passwordKey, password);
+    return await _getPrefs()
+        .then((prefs) => prefs.setString(_passwordKey, password));
   }
 
   Future<bool> saveSession(
       AuthToken authToken, String email, String password) async {
-    return await _saveAuthToken(authToken) &&
-        await _saveEmail(email) &&
-        await _savePassword(password);
+    return Future.wait([
+      _saveAuthToken(authToken),
+      _saveEmail(email),
+      _savePassword(password),
+    ]).then((_) => true).catchError(() => false);
   }
 
   Future<AuthToken> getAuthToken() async {
-    final prefs = await SharedPreferences.getInstance();
-    final String authTokenJson = await prefs.get(_authTokenKey);
+    final String authTokenJson =
+        await _getPrefs().then((prefs) => prefs.get(_authTokenKey));
 
     if (authTokenJson == null) {
       return null;
@@ -51,40 +59,50 @@ class _SessionManager {
     }
   }
 
-  Future<String> getEmail() async {
-    final prefs = await SharedPreferences.getInstance();
-    return await prefs.get(_emailKey);
-  }
+  Future<String> getEmail() async =>
+      await _getPrefs().then((prefs) => prefs.get(_emailKey));
 
-  Future<String> getPassword() async {
-    final prefs = await SharedPreferences.getInstance();
-    return await prefs.get(_passwordKey);
-  }
+  Future<String> getPassword() async =>
+      await _getPrefs().then((prefs) => prefs.get(_passwordKey));
 
-  Future<bool> deleteSession() async {
-    final prefs = await SharedPreferences.getInstance();
-    return await prefs.clear();
-  }
+  Future<bool> deleteSession() async =>
+      await _getPrefs().then((prefs) => prefs.clear());
 
   Future<bool> isLoggedIn() async {
-    final authToken = await getAuthToken();
+    final AuthToken authToken = await getAuthToken();
     return authToken != null ? true : false;
   }
 
-  Future<bool> _saveFareToken(Fare fare) async {
-    final prefs = await SharedPreferences.getInstance();
-    return await prefs.setString(_fareTokenKey, fare.promiseToken);
+  Future<bool> _saveSelectedVehicle(Vehicle vehicle) async {
+    final String vehicleJson = json.encode(vehicle);
+    return await _getPrefs()
+        .then((prefs) => prefs.setString(_selectedVehicleKey, vehicleJson));
+  }
+
+  Future<bool> _saveSelectedZoneToken(MunicipalZone zone) async {
+    return await _getPrefs()
+        .then((prefs) => prefs.setString(_selectedFareTokenKey, zone.token));
+  }
+
+  Future<bool> _saveSelectedFareToken(Fare fare) async {
+    return await _getPrefs().then(
+        (prefs) => prefs.setString(_selectedFareTokenKey, fare.promiseToken));
   }
 
   Future<bool> _saveSelectedFares(List<FareCost> selectedFares) async {
-    final prefs = await SharedPreferences.getInstance();
     final String selectedFaresJson = json.encode(selectedFares);
-    return await prefs.setString(_selectedFaresKeys, selectedFaresJson);
+    return await _getPrefs().then(
+        (prefs) => prefs.setString(_selectedFaresKeys, selectedFaresJson));
   }
 
-  Future<bool> saveParkingSession(Fare fare) async {
-    return await _saveFareToken(fare) &&
-        await _saveSelectedFares(fare.getSelectedFares());
+  Future<bool> saveParkingSession(
+      Vehicle vehicle, MunicipalZone zone, Fare fare) async {
+    return Future.wait([
+      _saveSelectedVehicle(vehicle),
+      _saveSelectedZoneToken(zone),
+      _saveSelectedFareToken(fare),
+      _saveSelectedFares(fare.getSelectedFares()),
+    ]).then((_) => true).catchError(() => false);
   }
 
   Future<bool> updateSelectedFares() async {
@@ -97,21 +115,33 @@ class _SessionManager {
     if (selectedFares.length > 0) {
       selectedFares.removeAt(0);
     } else {
-      final prefs = await SharedPreferences.getInstance();
-      return await prefs.setString(_selectedFaresKeys, null);
+      return await _getPrefs()
+          .then((prefs) => prefs.setString(_selectedFaresKeys, null));
     }
 
     return await _saveSelectedFares(selectedFares);
   }
 
-  Future<String> getFareToken() async {
-    final prefs = await SharedPreferences.getInstance();
-    return await prefs.get(_fareTokenKey);
+  Future<Vehicle> getSelectedVehicle() async {
+    final String vehicleJson =
+        await _getPrefs().then((prefs) => prefs.get(_selectedVehicleKey));
+
+    if (vehicleJson == null) {
+      return null;
+    } else {
+      return Vehicle.fromJson(json.decode(vehicleJson));
+    }
   }
 
+  Future<String> getSelectedZoneToken() async =>
+      await _getPrefs().then((prefs) => prefs.get(_selectedZoneTokenKey));
+
+  Future<String> getSelectedFareToken() async =>
+      await _getPrefs().then((prefs) => prefs.get(_selectedFareTokenKey));
+
   Future<List<FareCost>> getSelectedFares() async {
-    final prefs = await SharedPreferences.getInstance();
-    final String selectedFaresJson = await prefs.get(_selectedFaresKeys);
+    final String selectedFaresJson =
+        await _getPrefs().then((prefs) => prefs.get(_selectedFaresKeys));
 
     if (selectedFaresJson == null) {
       return null;
